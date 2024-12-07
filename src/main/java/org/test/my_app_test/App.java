8d276053d227f630f6844220ac5d5f54f76e7b0c
more3118 <67715407+more3118@users.noreply.github.com>
2024-12-06 19:03:29 -0600

    Update App.java

--- src/main/java/org/test/my_app_test/App.java
+++ src/main/java/org/test/my_app_test/App.java
@@ -27,7 +27,7 @@ public class App {
 
 		// 1. Open the URL
 		// driver.get("http://13.58.166.105:8080/addressbook-2.0/");
-		driver.get("http://3a4b6134d2534cdea78206a8a732729f2c.mylabserver.com:8081/contact.html"); //testserver
+		driver.get("http://52.53.159.130.com:8081/contact.html"); //testserver
 		// driver.get("http://3a4b6134d2534cdea78206a8a732729f3c.mylabserver.com/contact.html"); //prod
 		// invoke implicit wait
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
